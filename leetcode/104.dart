250291a14e970ff076a29b2d066178b3275ab972
김서후 <ksh20531@gmail.com>
2024-11-11 10:43:16 +0900

    241111 [leetcode] 104 이진 트리의 최대 depth // 추가 학습 필요

---
+++ leetcode/104.dart
@@ -0,0 +1,10 @@
+class Solution {
+  int maxDepth(TreeNode? root) {
+    if (root == null) {
+      return 0;
+    }
+    int leftDepth = maxDepth(root.left);
+    int rightDepth = maxDepth(root.right);
+    return 1 + (leftDepth > rightDepth ? leftDepth : rightDepth);
+  }
+}
